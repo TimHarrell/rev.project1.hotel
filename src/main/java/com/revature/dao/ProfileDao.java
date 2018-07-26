ca54ff191b85c65151b1b4cf79f205afc5a9f85f
Tim <timothy_harrell@icloud.com>
2018-07-25 22:39:27 -0500

    login

--- src/main/java/com/revature/dao/ProfileDao.java
+++ src/main/java/com/revature/dao/ProfileDao.java
@@ -1,9 +1,11 @@
 package com.revature.dao;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.ArrayList;
 import java.util.List;
 import com.revature.beans.Profile;
@@ -12,8 +14,8 @@ import com.revature.util.ConnectionUtil;
 
 public class ProfileDao {
 	/*
-	 * needs work
 	 * it grabs an account from the database, only checking if it exists
+	 * REMEMBER TO ADD STUFF FOR INVALIDCHARACTERS
 	 */
 	public static Profile getProfile(String userIdIn, String passwordIn) {
 		System.out.println("connecting...");
@@ -67,10 +69,38 @@ public class ProfileDao {
 	
 	/*
 	 * needs works
-	 * checks to see if the userId and the provided password are a match,
-	 * otherwise it will return false
+	 * REMEMBER TO ADD STUFF FOR INVALIDCHARACTERS, make the escape character invalid
 	 */
-	public static Boolean validateProfileLogin(String userId, String password) {
-		return false;
+	public static Boolean makeProfile(String userId, String firstname, String lastname, String password) {
+		CallableStatement cs = null;
+		boolean success = false;
+		System.out.println("connecting...");
+		try(Connection conn = ConnectionUtil.getConnection()) {
+			String sql = "INSERT INTO PROFILES (userID, firstname, lastname, passwrd) VALUES (?, ?, ?, ?)";
+			cs = conn.prepareCall(sql);
+			cs.setString(1, userId);
+			cs.setString(2, firstname);
+			cs.setString(3, lastname);
+			cs.setString(4, password);
+			
+			Boolean result = cs.execute();
+			if (!result) {
+				System.out.println("Successful account submission.");
+				success = true;
+			} else {
+				System.out.println("Failed");
+				success = false;
+			}
+			
+			cs.close();
+		} catch(SQLIntegrityConstraintViolationException ex) {
+			System.out.println("That username already exists.");
+			success = false;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			success = false;
+		}
+		
+		return success;
 	}
 }
